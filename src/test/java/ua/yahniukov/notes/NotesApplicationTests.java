1b418cd405fc65fc1ef103bc4275b246d09e630e
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2022-07-20 11:55:05 +0300

    feat: first commit

---
+++ src/test/java/ua/yahniukov/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.yahniukov.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
